ff80cbeed0b8582c3b3b2cb63cbca570688ea0d1
Costantino Giuliodori <costantino.giuliodori@gmail.com>
2013-10-20 11:26:58 +0200

    cancellazione in terapia valutazione

--- app/scripts/TerapiaValutazioneCtrl.js
+++ app/scripts/TerapiaValutazioneCtrl.js
@@ -2,29 +2,34 @@ var TerapiaValutazioneCtrl = [
   '$scope','$routeParams', 
   '$timeout', '$http','loadDataListIntoScope',
   '$window','$log',
-    function($scope, $routeParams,
-        $timeout, $http,loadDataListIntoScope,
+    function($scope, $routeParams, $timeout, $http, loadDataListIntoScope,
         $window, $log) {
+
+      var url_tv = '/data/pazienti/' + $routeParams.idPaziente + '/terapia_valutazione';
 
       // init: non c'è isogno che sia esposta allo $scope
       function init() {
-        debugger;
         $scope.formState    = {};
         $scope.tv_aggiungi  = {};
 
-        $http.get('/data/pazienti/' + $routeParams.idPaziente + '/terapia_valutazione')
+        $http.get(url_tv)
               .success(function(data, status, headers, config) {
                 $scope.terapia_valutazione = data;
               })
               .error(function(data, status, headers, config) {
-                $log.error('lettura di /data/pazienti/' + $routeParams.idPaziente + '/terapia_valutazione non riuscita!');
+                $log.error('lettura di '+ url_tv +' non riuscita!');
               });
       }
       
-      $scope.elimina_t = function(t) {
-        $window._.remove($scope.terapie_valutazione, function(tv) {
-          return tv.tempo === t.tempo;
-        });
+      $scope.elimina_tv = function(t) {
+        $http.post(url_tv + '/' + t.tempo + '/cancella', 
+                   {id_paziente: $routeParams.idPaziente, tempo: t.tempo}) 
+          .success(function(data, status, headers, config) {
+            init();
+          })
+          .error(function(data, status, headers, config) {
+            $log.error('errore nella cancellazione di\n' + t);
+          });
       };
      
       $scope.canSave = function() {
@@ -71,14 +76,12 @@ var TerapiaValutazioneCtrl = [
         // disabilito l'aggiungi
         $scope.formState.saving = true;
 
-        $scope.tv_aggiungi.id_paziente =  $routeParams.idPaziente;
+        $scope.tv_aggiungi.id_paziente = $routeParams.idPaziente;
 
-        $http.post('/data/pazienti/' + $routeParams.idPaziente + '/terapia_valutazione',
-          $scope.tv_aggiungi ).
+        $http.post(url_tv, $scope.tv_aggiungi ).
             success(function(data, status, headers, config) {
               $log.info('salvataggio avvenuto con successo');
               $scope.formState.saving = false;
-              debugger;
               init();
             }).
             error(function(data, status, headers, config) {              
