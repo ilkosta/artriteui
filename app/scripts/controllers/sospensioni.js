ced96b392ed7f304473ac083107d0b78f00e1796
Costantino Giuliodori <costantino.giuliodori@gmail.com>
2013-12-09 17:22:13 +0100

    wip...

--- app/scripts/controllers/sospensioni.js
+++ app/scripts/controllers/sospensioni.js
@@ -11,11 +11,43 @@
         loadDataListIntoScope) {
 
       var dataUrl = {};
+      var editForm = {
+            cod_tipo_sospensione: ''
+          , id_motivo_sospensione: null
+          , id_sospensione_dettaglio: null
+          , data_sospensione: null
+          , annotazioni: null
+          , setMotivoSospensione: function(v) { this.id_motivo_sospensione = v; }
+          , setMotivoSospensione2: function(v) { this.id_sospensione_dettaglio = v; }
+          };
+
+      var sospensioni_mngr = function(data) {
+        var grouped = _.groupBy(data,'sospensione');
+
+        return {
+          edit: editForm,
+
+          getMotiviSospensione: function() { 
+            return _(grouped).keys().map(function(k) {
+              return {id: k, desc: grouped[k][0]['sospensione']};
+            }).value(); 
+          },
+          getMotiviSospensione2: function() { 
+            if(!this.edit.id_motivo_sospensione) return [];
+            var ret = _(grouped[this.edit.id_motivo_sospensione])
+              .filter(function(i){ return i.id_sospensione_dettaglio !== null; })
+              .map(function(i) { return {id: i.id_sospensione_dettaglio, desc: i.id_sospensione_dettaglio}; })
+              .value();
+            return ret;
+          }
+        };
+      };
+
 
       var init = function() {
         loadDataListIntoScope(
           $scope,
-          [ 'cod_tipo_sospensione' , 'tipo_sospensione' ],
+          [ 'cod_tipo_sospensione'],
           function(p) { return '/data/_' + p;}
         ); 
 
@@ -24,6 +56,7 @@
         };
 
         $scope.formState = {};
+        $scope.edit_sospensione = {};
         
         // init di $scope.sospensioni
         $http.get(dataUrl.sospensioni)
@@ -35,14 +68,22 @@
             $scope.sospensioni = [];
           });
 
-        
+        $http.get('/data/_tipo_sospensione')
+          .success(function(data, status, headers, config) {
+            $scope.tipo_sospensione = sospensioni_mngr(data);
+            console.log("tipo sospensione:");
+            console.log($scope.tipo_sospensione.getMotiviSospensione());
+          })
+          .error(function(data, status, headers, config) {
+            growl.addErrorMessage("Errore nella lettura delle sospensioni del paziente.\nControlla la connessione al server!");
+            $scope.tipo_sospensione = {};
+          });
+
       };
 
       init();
 
-      // -------------------------
-      $scope.setTipoSospensione = function(value) { $scope.nuova_sospensione.cod_tipo_sospensione = value; };
-
     }
   ]);
 })();
+
