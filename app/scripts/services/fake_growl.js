2af402d0038f0d58686f0e3eaabdd152ed7c6a77
ilkosta <costantino.giuliodori@gmail.com>
2014-02-10 21:01:23 +0100

    rimosso jquery

---
+++ app/scripts/services/fake_growl.js
@@ -0,0 +1,10 @@
+;(function () {
+  'use strict';
+
+  angular.module('angular-growl', []).factory('growl', function() {
+    return {
+      addErrorMessage: function(str) {},
+      addSuccessMessage: function(str) {}
+    };
+  });
+}());
