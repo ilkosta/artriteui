aedca7885cd8a2b92e48c0bafcca1c68a9362a9f
Costantino Giuliodori <costantino.giuliodori@gmail.com>
2014-02-24 20:49:14 +0100

    inserito controllo dell'esistenza dei test delle dipendenze tramite after-brunch

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -2,75 +2,78 @@
 // Generated on Fri Feb 21 2014 17:31:12 GMT+0100 (CET)
 
 module.exports = function(config) {
-  config.set({
+    config.set({
 
-    // base path, that will be used to resolve files and exclude
-    basePath: '..',
-
-
-    // frameworks to use
-    frameworks: ['jasmine'],
+        // base path, that will be used to resolve files and exclude
+        basePath: '..',
 
 
-    // list of files / patterns to load in the browser
-    files: [
-      'server/public/js/vendor.js', 
-      'bower_components/angular-mocks/angular-mocks.js',
-      'server/public/js/app.templates.js', 
-      'server/public/js/app.js', 
-      'test/unit/*.spec.js'
-    ],
+        // frameworks to use
+        frameworks: ['jasmine'],
 
 
-    // list of files to exclude
-    exclude: [
-      
-    ],
+        // list of files / patterns to load in the browser
+        files: [
+
+            'server/public/js/vendor.js',
+            'bower_components/angular-mocks/angular-mocks.js',
+            'server/public/js/app.templates.js',
+            'server/public/js/app.js',
+            'test/dependency_tests.js',
+            'test/unit/*.spec.js',
+            'test/dependencies/*.spec.js'
+        ],
 
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['progress'],
+        // list of files to exclude
+        exclude: [
+
+        ],
 
 
-    // web server port
-    port: 9876,
+        // test results reporter to use
+        // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+        reporters: ['progress'],
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // web server port
+        port: 9876,
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
 
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera (has to be installed with `npm install karma-opera-launcher`)
-    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
-    // - PhantomJS
-    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-    browsers: [
-    //    'Firefox', 
-    //    'Chrome', 
-        'PhantomJS'
-    ],
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
 
-    // If browser does not capture in given timeout [ms], kill it
-    captureTimeout: 60000,
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera (has to be installed with `npm install karma-opera-launcher`)
+        // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+        // - PhantomJS
+        // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+        browsers: [
+            'Firefox'
+            //,    'Chrome'
+            //,'PhantomJS'
+        ],
 
 
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: false
-  });
+        // If browser does not capture in given timeout [ms], kill it
+        captureTimeout: 60000,
+
+
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: false
+    });
 };
